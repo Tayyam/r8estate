3641de8c69de1cfccff797a49bb687194912a209
Tayyam <115746736+Tayyam@users.noreply.github.com>
2025-07-14 14:30:17 +0300

    Updated UnverifiedUserView.tsx

--- src/components/UnverifiedUserView.tsx
+++ src/components/UnverifiedUserView.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { AlertOctagon, Mail } from 'lucide-react';
 import { useLanguage } from '../contexts/LanguageContext';
 import { useAuth } from '../contexts/AuthContext';
