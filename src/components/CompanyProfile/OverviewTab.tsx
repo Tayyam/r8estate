5b308b620c4e425b9021ac962ea80b35f6720f2e
Tayyam <115746736+Tayyam@users.noreply.github.com>
2025-07-15 10:03:28 +0300

    Updated OverviewTab.tsx

--- src/components/CompanyProfile/OverviewTab.tsx
+++ src/components/CompanyProfile/OverviewTab.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState ,useEffect} from 'react';
 import { useLanguage } from '../../contexts/LanguageContext';
 import { useAuth } from '../../contexts/AuthContext';
 import { collection, query, where, getDocs } from 'firebase/firestore';
